347bbf00feb1b78f1c5d6606831b4f1ce65046a8
Leonardo da Silva <lslv1243@gmail.com>
2021-06-02 18:03:51 -0300

    actually make it work

--- android/src/main/kotlin/com/example/is_screen_round/IsScreenRoundPlugin.kt
+++ android/src/main/kotlin/com/example/is_screen_round/IsScreenRoundPlugin.kt
@@ -2,6 +2,7 @@ package com.example.is_screen_round
 
 import androidx.annotation.NonNull
 import android.content.res.Configuration
+import android.content.res.Resources
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -16,15 +17,17 @@ class IsScreenRoundPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var resources: Resources
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "is_screen_round")
     channel.setMethodCallHandler(this)
+    resources = flutterPluginBinding.applicationContext.resources
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "isScreenRound") {
-       val config: Configuration = resources.configuration
+        val config: Configuration = resources.configuration
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
             result.success(config.isScreenRound)
         } else {
